47d578f4f519456255972917b2ee6ab164d66b47
Drurivera <riv18178@uvg.edu.gt>
2021-04-26 13:22:25 -0600

    Imagenes proyecto

--- LAB_5/lab_5_18178/lab_5_18178.ino
+++ LAB_5/lab_5_18178/lab_5_18178.ino
@@ -8,8 +8,8 @@
 #include <SPI.h>
 #include <SD.h>
 
-String x = "";
 
+int nombre = Serial.read();
 File root;
 File myFile;
 
@@ -31,115 +31,106 @@ void setup()
   Serial.println("");
   Serial.println(" MENU DE OPCIONES PARA IMPRIMIR ");
   Serial.println("");
-  Serial.println("-PARA CORAZON ESCRIBIR 'COR1' ");
-  Serial.println("");
-  Serial.println("-PARA CORAZON ESCRIBIR 'DIN2' ");
-  Serial.println("");
-  Serial.println("-PARA CORAZON ESCRIBIR 'HON3' ");
-  Serial.println("");
-  Serial.println("or END to finish"); 
-
+  root = SD.open("/");
+  printDirectory(root, 0);  
+  nombre = 0;
 }
  void loop(){
     //OBTENIDO CODIGO PARA LEER CADENAS DE https://www.luisllamas.es/cadenas-de-texto-puerto-serie-arduino/
   while (Serial.available()){
-      char character = Serial.read();//guarda lo que se lee del puerto 
-      if (character != '\n') // si character no es igual a nada
-      {
-         x.concat(character); //concatena los valores de character en una cadena
-      }
-       else
-      {
-         x = "";
-      }
+      nombre = Serial.read();//guarda lo que se lee del puerto 
+      casos ();
    }
-   if(x=="COR1"){
-    // re-open the file for reading:
-    myFile = SD.open("CORAZON.txt");
-    if (myFile) {
-      Serial.println("Archivo:");
-      // read from the file until there's nothing else in it:
-      while (myFile.available()) {
-        Serial.write(myFile.read());
-      }
-      // close the file:
-      myFile.close();
-      Serial.println("");
-      Serial.println(" MENU DE OPCIONES PARA IMPRIMIR ");
-      Serial.println("");
-      Serial.println("-PARA CORAZON ESCRIBIR 'COR1' ");
-      Serial.println("");
-      Serial.println("-PARA CORAZON ESCRIBIR 'DIN2' ");
-      Serial.println("");
-      Serial.println("-PARA CORAZON ESCRIBIR 'HON3' ");
-      Serial.println("");
-      Serial.println("or END to finish");  
-      return;
-    } 
-    else {
-      // if the file didn't open, print an error:
-      Serial.println("error opening test.txt");
-      return;
-    }
-  }
-
-   if(x=="DIN2"){
-    // re-open the file for reading:
-    myFile = SD.open("DINO.txt");
-    if (myFile) {
-      Serial.println("Archivo:");
-      // read from the file until there's nothing else in it:
-      while (myFile.available()) {
-        Serial.write(myFile.read());
-      }
-      // close the file:
-      myFile.close();
-      Serial.println("");
-      Serial.println(" MENU DE OPCIONES PARA IMPRIMIR ");
-      Serial.println("");
-      Serial.println("-PARA CORAZON ESCRIBIR 'COR1' ");
-      Serial.println("");
-      Serial.println("-PARA CORAZON ESCRIBIR 'DIN2' ");
-      Serial.println("");
-      Serial.println("-PARA CORAZON ESCRIBIR 'HON3' ");
-      Serial.println("");
-      Serial.println("or END to finish");   
-      return;
-    } 
-    else {
-      // if the file didn't open, print an error:
-      Serial.println("error opening test.txt");
-      return;
-    }
-  }
-
-  if(x=="HON3"){
-    // re-open the file for reading:
-    myFile = SD.open("HONGO.txt");
-    if (myFile) {
-      Serial.println("Archivo:");
-      // read from the file until there's nothing else in it:
-      while (myFile.available()) {
-        Serial.write(myFile.read());
-      }
-      // close the file:
-      myFile.close();
-      Serial.println("");
-      Serial.println(" MENU DE OPCIONES PARA IMPRIMIR ");
-      Serial.println("");
-      Serial.println("-PARA CORAZON ESCRIBIR 'COR1' ");
-      Serial.println("");
-      Serial.println("-PARA CORAZON ESCRIBIR 'DIN2' ");
-      Serial.println("");
-      Serial.println("-PARA CORAZON ESCRIBIR 'HON3' ");
-      Serial.println("");
-      Serial.println("or END to finish");  
-      return;
-    } 
-    else {
-      // if the file didn't open, print an error:
-      Serial.println("error opening test.txt");
-      return;
-    }
-  }
  }
+ void casos(){ 
+  printDirectory(root, 0);
+  if(nombre == 49){
+    // re-open the file for reading:
+    myFile = SD.open("1_DINO.txt");
+    if (myFile) {
+      Serial.println("Archivo:");
+      // read from the file until there's nothing else in it:
+      while (myFile.available()) {
+        Serial.write(myFile.read());
+      }
+      // close the file:
+      myFile.close();
+      nombre = 0;
+    } 
+    else {
+      // if the file didn't open, print an error:
+      Serial.println("error opening 1_DINO.txt");
+      return;
+    }
+  }
+
+  if(nombre == 50){
+    // re-open the file for reading:
+    myFile = SD.open("2_HONGO.txt");
+    if (myFile) {
+      Serial.println("Archivo:");
+      // read from the file until there's nothing else in it:
+      while (myFile.available()) {
+        Serial.write(myFile.read());
+      }
+      // close the file:
+      myFile.close();
+      nombre = 0;
+      return;
+    } 
+    else {
+      // if the file didn't open, print an error:
+      Serial.println("error opening DINO.txt");
+      return;
+    }
+  }
+
+  if(nombre == 51){
+    // re-open the file for reading:
+    myFile = SD.open("3_CORA~1.txt");
+    if (myFile) {
+      Serial.println("Archivo:");
+      // read from the file until there's nothing else in it:
+      while (myFile.available()) {
+        Serial.write(myFile.read());
+      }
+      // close the file:
+       myFile.close();
+      nombre = 0;
+      return;
+    } 
+    else {
+      // if the file didn't open, print an error:
+      Serial.println("error opening 3_CORA~1.txt");
+      return;
+    }
+  }
+   if(nombre == 48){
+    Serial.println("aqui estoy");
+    return;
+    }
+   }
+ void printDirectory(File dir, int numTabs) {
+   while(true) {
+     
+     File entry =  dir.openNextFile();
+     if (! entry) {
+       // no more files
+       //dir.rewindDirectory();
+       break;
+     }
+     for (uint8_t i=0; i<numTabs; i++) {
+       Serial.print('\t');
+     }
+     Serial.print(entry.name());
+     if (entry.isDirectory()) {
+       Serial.println("/");
+       printDirectory(entry, numTabs+1);
+     } else {
+       // files have sizes, directories do not
+       Serial.print("\t\t");
+       Serial.println(entry.size(), DEC);
+     }
+     entry.close();
+   }
+}
