a4b47c3747ae9d4ead1347a39b2c3763745d0580
Drurivera <riv18178@uvg.edu.gt>
2021-05-01 01:03:39 -0600

    Proyecto 3 completo con video

--- Proyecto 3/dino_progra/dino_progra.ino
+++ Proyecto 3/dino_progra/dino_progra.ino
@@ -18,7 +18,9 @@
 UTFTGLUE myGLCD(0,A2,A1,A3,A4,A0); //all dummy args
  int pushT;
  int pushTE = 10;
-
+ int Run = 0;
+ int Bloque = 0;
+ int Action = 1;
 // Declare which fonts we will be using
 //extern uint8_t SmallFont[];      //GLUE defines as GFXFont ref
 
@@ -52,21 +54,23 @@ void setup()
 void loop(){
    Serial.write(1);
    pushT = Serial.read();
-   
    Home();
    delay (1000);
    if (digitalRead(pushTE) == 0){
     myGLCD.fillScr(0,0,0); //coloca el fondo en negro
-    int Action = 1;
+    Action = 1;
     while (Action == 1){
       Juego();
       Runner();
-      delay(250);
-      Block ();
-      delay(500);
-      myGLCD.fillScr(0,0,0); //coloca el fondo en negro
+      MoveB ();
+      if (digitalRead(pushTE) == 0){
+        SaltoRunner();
+        MoveB ();
       }
+      Perder ();
     }
+    return;
+}
 }  
 void Home(){
   //myGLCD.clrScr(); //limpia pantalla
@@ -77,14 +81,27 @@ void Home(){
   myGLCD.setBackColor(255,255,255); //fondo de la letra 
   myGLCD.print("Bienvenido a Dino Run", CENTER, 93);
   myGLCD.print("Presiona el push para iniciar", CENTER, 119);
-  
+  Action = 1;
   myGLCD.setColor(255, 255, 255); //color de la letra
   myGLCD.setBackColor(0,0,0); //fondo de la letra 
   myGLCD.print("ESTAS LISTO?", CENTER, 180);
 }
 
-void Juego(){
+void Perder (){
 
+  if (((Bloque<=-120)&&(Bloque)>=-220) && Run == 0){
+    
+    myGLCD.fillScr(0,0,0);
+    Gameover();
+    delay(2000);
+    myGLCD.fillScr(0,0,0);
+
+    }
+  return;
+  
+  } 
+
+void Juego(){
   myGLCD.setColor(14, 95, 166); //color del rectangulo del titulo
   myGLCD.fillRect(0, 0, 319, 13); //coordenadas donde se debe imprimir el rectangulo
   myGLCD.fillRect(0, 226, 319, 239); 
@@ -99,11 +116,7 @@ void Juego(){
   myGLCD.setColor(255, 255, 255);
   myGLCD.fillRect(0, 196, 319, 201);  
 
-    for (int i=0; i<2250; i++)
-  {
-    myGLCD.setColor(102,102,255);
-    myGLCD.drawPixel(2+random(316), 16+random(180));
-  }
+
 }
 void Gameover(){
   myGLCD.fillScr(0,0,0);
@@ -117,21 +130,81 @@ void Gameover(){
   myGLCD.setColor(255, 255, 255);
   myGLCD.setBackColor(255,255,255);
   myGLCD.print("VUELVETE A RETAR?", CENTER, 180);
+  Action = 0;
+  Run = 0;
+  Bloque = 0;
   
-  delay (10000);
+  //delay (10000);
+  return;
 
 }
-
 void Runner(){
    myGLCD.setColor(255, 255, 255);
-   myGLCD.fillRoundRect(110, 130, 120,195);
-   myGLCD.fillRoundRect(110, 185, 160,195);
-   myGLCD.fillRoundRect(100, 160, 140,170);
-   myGLCD.fillRoundRect(100, 140, 140,150);
+   myGLCD.fillRoundRect(110, 130+Run, 120,195+Run);
+   myGLCD.fillRoundRect(110, 185+Run, 155,195+Run);
+   myGLCD.fillRoundRect(100, 160+Run, 140,170+Run);
+   myGLCD.fillRoundRect(100, 140+Run, 140,150+Run);
+}
+void LimpiarRunner(){
+   myGLCD.setColor(0,0,0);
+   myGLCD.fillRoundRect(110, 130+Run, 120,195+Run);
+   myGLCD.fillRoundRect(110, 185+Run, 155,195+Run);
+   myGLCD.fillRoundRect(100, 160+Run, 140,170+Run);
+   myGLCD.fillRoundRect(100, 140+Run, 140,150+Run);
+
+}
+void SaltoRunner(){
+      
+      Run = 0;
+      Runner();
+      delay(250);
+      MoveB ();
+      LimpiarRunner();
+      Run = -50;     
+      Runner();
+      delay(500);
+      MoveB ();
+      //Perder ();
+      LimpiarRunner();
+      Run = -100;       
+      Runner();
+      delay(500);
+      MoveB ();
+      LimpiarRunner();
+      Run = -50;
+      //Perder ();       
+      Runner();
+      MoveB ();
+      MoveB ();
+      LimpiarRunner();
+      Run = 0;     
+      Runner();
+} 
+void Block (){
+  myGLCD.setColor(0, 255, 255);
+  myGLCD.fillRoundRect(280+Bloque, 160, 320+Bloque,195);
+  }
+
+void LimpiarBlock (){
+  myGLCD.setColor(0,0,0);
+  myGLCD.fillRoundRect(280+Bloque, 160, 320+Bloque,195);
+  }
+
+void MoveB (){
+      /*
+      Bloque = 0;
+      Block();
+      delay(250);
+ */
+      LimpiarBlock();
+      Bloque = Bloque -30;
+      if(Bloque <= -270){
+        Bloque = 0;
+        }
+      Block();
+      delay(450);
+      
 
 }
 
-void Block (){
-  myGLCD.setColor(0, 255, 255);
-  myGLCD.fillRoundRect(180, 160, 230,195);
-  }
+  
